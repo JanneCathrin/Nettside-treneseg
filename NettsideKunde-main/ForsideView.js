f229eb2fa17516e78f42a6e73c50e1de6dd892ca
JanneCathrin <77385680+JanneCathrin@users.noreply.github.com>
2022-01-17 00:28:55 +0100

    Add files via upload

---
+++ NettsideKunde-main/ForsideView.js
@@ -0,0 +1,45 @@
+function updateViewForside(){
+    let html = `
+
+    
+<div class="bg-img">
+  <div class="container">
+    <div class="topnav">
+      <a href="#home">RIOT GAMES ''LOGO'' ▼</a>
+      <a href="#who">WHO ARE WE ▼</a>
+      <a href="#Work">WORK WITH US ▼</a>
+      <a href="#News">NEWS</a>
+      <input type="text" placeholder="Search" aria-label="Search through site content"name="search">
+      <div class="buttonibilde">⮊ LEARN MORE</div>
+    </div>
+  </div>
+</div>
+
+
+    
+     
+   
+   
+   
+   
+  </div>
+
+    <div class="header">
+    
+    </div>
+
+
+    <div class="main">
+
+    </div>
+
+
+
+      
+      <div class="footer">
+        
+      </div>
+    
+    `;
+    document.getElementById('app').innerHTML = html;
+}
